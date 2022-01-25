c7420b44bae850cbb6e12fdc514c5b70be9036bc
nitaku <matteo.abrate@gmail.com>
2022-01-25 23:28:31 +0100

    WIP viewport filtering for bubble labels

--- src/BubblePack.svelte
+++ src/BubblePack.svelte
@@ -1,12 +1,27 @@
 <script>
-	import { current_transform } from 'anymapper'
+	import { current_transform, viewport } from 'anymapper'
+import View from 'anymapper/src/View.svelte';
 	import Bubble from './Bubble.svelte'
 	import BubbleLabel from './BubbleLabel.svelte'
 
 	export let data
 	export let bubble_color
 
-	$:z = $current_transform.k
+	$: z = $current_transform.k
+
+	function should_bubble_be_visibile(d, zoom) {
+		const is_synset = d.data.original_node.type == 'synset'
+		const is_in_zoom_range = zoom >= 3/d.r
+		return is_synset && is_in_zoom_range
+	}
+
+	function should_bubble_label_be_visible(d, zoom) {
+		const is_synset = d.data.original_node.type == 'synset'
+		const is_in_zoom_range = d.r > 30/zoom && d.r < 120/zoom
+		const is_in_viewport = d.x > $viewport.left && d.x < $viewport.right && d.y > $viewport.top && d.y < $viewport.bottom
+		const is_parent_not_in_zoom_range = d.parent && (d.parent.r <= 30/zoom || d.parent.r >= 120/zoom)
+		return is_synset && is_in_zoom_range && is_in_viewport && is_parent_not_in_zoom_range
+	}
 </script>
 
 <style>
@@ -14,12 +29,12 @@
 
 <g>
 	{#each data as d}
-		{#if d.data.original_node.type == 'synset' && z >= 3/d.r}
+		{#if should_bubble_be_visibile(d, z)}
 			<Bubble {d} {bubble_color}/>
 		{/if}
 	{/each}
 	{#each data as d}
-		{#if d.data.original_node.type == 'synset' && d.r > 30/z && d.r < 120/z && d.parent && (d.parent.r <= 30/z || d.parent.r >= 120/z) }
+		{#if should_bubble_label_be_visible(d, z)}
 			<BubbleLabel {d}/>
 		{/if}
 	{/each}
