9ffa57f435c431e45ae3118b9111e5541a5fe23b
nitaku <matteo.abrate@gmail.com>
2022-01-26 11:53:53 +0100

    clickable link to synonyms

--- src/Info.svelte
+++ src/Info.svelte
@@ -6,11 +6,17 @@
     
 	export let d
 
-    function get_synset_children_lemmas(d) {
-        return d.data.children.filter(x => x.original_node.type == 'sense').map(x => x.original_node.lemma)
+    function get_synset_children(d) {
+        return d.data.children.filter(x => x.original_node.type == 'sense')
     }
-    function get_siblings_lemmas(d) {
-		return get_synset_children_lemmas(d.parent).filter(x => x != d.data.original_node.lemma)
+    function get_synset_children_lemmas(d) {
+        return get_synset_children(d).map(x => x.original_node.lemma)
+    }
+    function get_sense_siblings(d) {
+		return get_synset_children(d.parent).filter(x => x.original_node.type == 'sense' && x.original_node.id != d.data.original_node.id)
+	}
+    function get_sense_siblings_lemmas(d) {
+		return get_sense_siblings(d).map(x => x.original_node.lemma)
 	}
     function expand_pos(pos) {
         switch(pos) {
@@ -43,6 +49,9 @@
     .synonyms {
         font-style: italic;
     }
+    .synonyms > :not(:last-child)::after {
+        content: ', '
+    }
     .definition {
         font-style: italic;
     }
@@ -53,7 +62,8 @@
     <span class="pos">{expand_pos(d.data.original_node.pos)}</span>
     <span class="sensenum">{type == 'synset' ? '-' : d.data.original_node.sensenum+'.'}</span>
     {#if type == 'sense'}
-        <span class="synonyms">{ get_siblings_lemmas(d).join(', ') }:</span>
+        <span class="synonyms">
+            {#each get_sense_siblings(d) as sense_sibling}<span><a href="#{ sense_sibling.path }">{ sense_sibling.original_node.lemma }</a></span>{/each}:</span>
     {/if}
     <span class="definition">{type == 'synset' ? d.data.original_node.definition : d.parent.data.original_node.definition}.</span>
 </Content>
