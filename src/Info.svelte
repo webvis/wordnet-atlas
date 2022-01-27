96e84c9ed23e0bff41c07b19b9b39cccd201d806
nitaku <matteo.abrate@gmail.com>
2022-01-27 19:55:24 +0100

    simplistic hypernyms in info

--- src/Info.svelte
+++ src/Info.svelte
@@ -5,7 +5,7 @@
 
     $: type = d.data.original_node.type
     $: synonyms = get_sense_siblings(d)
-	
+	$: hypernyms = get_parent_senses(d)
 
     function get_synset_children(d) {
         return d.data.children.filter(x => x.original_node.type == 'sense')
@@ -16,6 +16,9 @@
     function get_sense_siblings(d) {
 		return get_synset_children(d.parent).filter(x => x.original_node.type == 'sense' && x.original_node.id != d.data.original_node.id)
 	}
+    function get_parent_senses(d) {
+        return d.parent.parent ? d.parent.parent.data.children.filter(x => x.original_node.type == 'sense') : []
+    }
     function expand_pos(pos) {
         switch(pos) {
             case 'n':
@@ -44,10 +47,10 @@
     .sensenum {
         font-weight: bold;
     }
-    .synonyms {
+    .synonyms, .hypernyms {
         font-style: italic;
     }
-    .synonyms > :not(:last-child)::after {
+    .synonyms > :not(:last-child)::after, .hypernyms > :not(:last-child)::after {
         content: ', '
     }
     .definition {
@@ -64,4 +67,10 @@
             {#each synonyms as synonym}<span><a href="#{ synonym.path }">{ synonym.original_node.lemma }</a></span>{/each}{#if synonyms.length > 0}:{/if}</span>
     {/if}
     <span class="definition">{type == 'synset' ? d.data.original_node.definition : d.parent.data.original_node.definition}.</span>
+    {#if type == 'sense'}
+        {#if hypernyms.length > 0}
+            <br><span>More generic: </span><span class="hypernyms">
+                {#each hypernyms as hypernym}<span><a href="#{ hypernym.path }">{ hypernym.original_node.lemma }</a></span>{/each}{#if hypernyms.length > 0}.{/if}</span>
+        {/if}
+    {/if}
 </Content>
